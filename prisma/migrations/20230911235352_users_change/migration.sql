18629fb16af1540e29ce754485e1f81e0f0be666
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-09-12 09:37:21 +0200

    removed

--- prisma/migrations/20230911235352_users_change/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "CmsUsers" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "isAdministrator" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "isEmployee" BOOLEAN NOT NULL DEFAULT false;
