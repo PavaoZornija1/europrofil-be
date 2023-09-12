853d0c67be5c64eb7ccac7845966070185212056
PavaoSemaZornija <pavao.zornija@sematext.com>
2023-09-12 02:25:09 +0200

    Employee

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,1 +1,33 @@
-export class CreateEmployeeDto {}
+import { ApprovalStatus, CmsDepartments } from '@prisma/client';
+import { IsString, IsNotEmpty, IsBoolean } from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public password: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  public isAdministrator: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  public address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public note: string;
+
+  @IsString()
+  public approvalStatus: ApprovalStatus;
+
+  public department: CmsDepartments;
+}
