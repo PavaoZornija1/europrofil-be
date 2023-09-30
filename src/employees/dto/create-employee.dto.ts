dd1f1b51e594844012ad586075c52a1f823e81b7
PavaoSemaZornija <pavao.zornija@sematext.com>
2023-09-30 21:47:23 +0200

    Add

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -12,30 +12,7 @@ export class CreateEmployeeDto {
 
   @IsString()
   @IsNotEmpty()
-  public phone: string;
-
-  @IsString()
-  @IsNotEmpty()
-  public username: string;
-
-  @IsString()
-  @IsNotEmpty()
   public password: string;
-
-  @IsBoolean()
-  @IsNotEmpty()
-  public isAdministrator: boolean;
-
-  @IsString()
-  @IsNotEmpty()
-  public address: string;
-
-  @IsString()
-  @IsNotEmpty()
-  public note: string;
-
-  @IsString()
-  public approvalStatus: ApprovalStatus;
 
   @IsOptional()
   public department: string;
