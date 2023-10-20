5027d4a7ba86f029ebddd80c62ac3d2160cde2b9
PavaoSemaZornija <pavao.zornija@sematext.com>
2023-10-20 04:05:05 +0200

    Update

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 @Injectable()
 export class OrdersService {
@@ -8,19 +11,50 @@ export class OrdersService {
     return 'This action adds a new order';
   }
 
-  findAll() {
-    return [];
+  async findAll() {
+    return await prisma.cmsOrders.findMany({
+      where: {
+        isActive: true,
+      },
+      include: {
+        cmsHandrail: true,
+        cmsHandrailDecoration: true,
+        cmsHandrailEnding: true,
+        cmsHorizontalProfile: true,
+        cmsMechanism: true,
+        cmsOrderDoors: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  findOne(id: string) {
+    return prisma.cmsOrders.findUnique({
+      where: { id: id },
+      include: {
+        cmsHandrail: true,
+        cmsHandrailDecoration: true,
+        cmsHandrailEnding: true,
+        cmsHorizontalProfile: true,
+        cmsMechanism: true,
+        cmsOrderDoors: true,
+      },
+    });
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
+  update(id: string, updateOrderDto: UpdateOrderDto) {
     return `This action updates a #${id} order`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async remove(id: string) {
+    return await prisma.cmsOrders.update({
+      where: {
+        id: id,
+      },
+      data: {
+        isActive: false,
+        // isDeleted: true,
+        deleted: new Date(),
+      },
+    });
   }
 }
